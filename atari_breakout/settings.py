8037fd27fedec04e90a3c5be55fa7bd12282ef68
Sayanth <sayanthstrng@gmail.com>
2026-04-10 17:51:43 +0530

    settings

---
+++ atari_breakout/settings.py
@@ -0,0 +1,32 @@
+# screen
+WIDTH = 800
+HEIGHT = 600
+
+# ball
+BALL_RADIUS = 10
+BALL_SPEED = 4
+
+# paddle
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
+PADDLE_SPEED = 6
+
+# bricks
+BRICK_WIDTH = 50
+BRICK_HEIGHT = 18
+GAP = 5
+
+ROWS = 6
+COLS = 12
+
+OFFSET_X = 20
+OFFSET_Y = 80
+
+# colors
+COLORS = [
+    (255, 0, 0),
+    (255, 165, 0),
+    (255, 255, 0),
+    (0, 255, 0),
+    (0, 200, 255)
+]
