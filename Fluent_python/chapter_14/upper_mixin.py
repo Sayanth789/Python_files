95ca1aa6ab8977b2b157448fb66c6a27414eb03b
Sayanth <sayanthstrng@gmail.com>
2026-06-19 18:45:51 +0530

    Create upper_mixin.py

---
+++ Fluent_python/chapter_14/upper_mixin.py
@@ -0,0 +1,44 @@
+import collections 
+
+def _upper(key):
+    try:
+        return key.upper()
+    except AttributeError:
+        return key
+    
+class UpperCaseMixin:
+    def __setitem__(self, key, item):
+        super().__setitem__(_upper(key), item)
+
+    def __getitem__(self, key):
+        return super().__getitem__(_upper(key))
+
+    def get(self, key, default=None):
+        return super().get(_upper(key), default)
+
+    def __contains__(self, key):
+        return super().__contains__(_upper(key))        
+
+class UpperDict(UpperCaseMixin, collections.UserDict):
+    pass 
+class UpperCounter(UpperCaseMixin, collections.Counter):
+    """ Specified 'counter' uppercase string keys """
+
+
+''' 
+UpperDict behaves like a case-insensisitive mapping
+
+
+'''
+d = UpperDict([('a', 'letter A'), (2, 'digit two')])
+print(list(d.keys()))
+
+d['b'] = 'letter B'
+print('b' in d)
+
+print(d['a'], d.get('B'))
+
+print(list(d.keys()))
+
+c = UpperCounter('BaNaNa')
+print(c.most_common)
